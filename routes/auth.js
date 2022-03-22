23cf47b8b30f3bde4c262202bfa870e1850add6a
Leonardo <leopanta99@gmail.com>
2022-03-22 10:33:25 -0400

    added more routes for the project api

--- routes/auth.js
+++ routes/auth.js
@@ -1,54 +1,49 @@
-const router = require('express').Router();
-const User = require('../model/User');
-const Validation = require('../validation');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const router = require("express").Router();
+const User = require("../model/User");
+const Validation = require("../validation");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-router.post('/register', async (req,res) => {
+router.post("/register", async (req, res) => {
+	const { error } = Validation.registerValidation(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
 
+	//checking user already exists
+	const emailExist = await User.findOne({ email: req.body.email });
+	if (emailExist) return res.status(400).send("EMAIL EXISTS");
 
-    const {error} = Validation.registerValidation(req.body);
-    if(error) return res.status(400).send(error.details[0].message);
+	const salt = await bcrypt.genSalt(10);
+	const hashPassword = await bcrypt.hash(req.body.password, salt);
 
-    //checking user already exists
-    const emailExist = await User.findOne({email: req.body.email})
-    if(emailExist) return res.status(400).send('EMAIL EXISTS')
-
-    const salt = await bcrypt.genSalt(10);
-    const hashPassword = await bcrypt.hash(req.body.password, salt);
-
-    const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: hashPassword,
-    });
-    try{
-        const savedUser = await user.save();
-        res.send(savedUser);
-    }catch(err){
-        res.status(400).send(err);
-        console.log(err);
-    }
+	const user = new User({
+		name: req.body.name,
+		email: req.body.email,
+		password: hashPassword,
+	});
+	try {
+		const savedUser = await user.save();
+		res.send(savedUser);
+	} catch (err) {
+		res.status(400).send(err);
+		console.log(err);
+	}
 });
 
+router.post("/login", async (req, res) => {
+	const { error } = Validation.loginValidation(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
 
-router.post('/login', async (req,res) => {
+	const user = await User.findOne({ email: req.body.email });
+	if (!user) return res.status(400).send("EMAIL IS INCORRECT");
 
-    const {error} = Validation.loginValidation(req.body);
-    if(error) return res.status(400).send(error.details[0].message);
+	const validPass = await bcrypt.compare(req.body.password, user.password);
+	if (!validPass) return res.status(400).send("PASSWORD IS INCORRECT");
 
-    const user = await User.findOne({email: req.body.email})
-    if(!user) return res.status(400).send('EMAIL IS INCORRECT')
+	//res.send('Logged in !!@!@!$#@$!%');
 
-    const validPass = await bcrypt.compare(req.body.password, user.password);
-    if(!validPass) return res.status(400).send('PASSWORD IS INCORRECT');
-
-    //res.send('Logged in !!@!@!$#@$!%');
-
-    //create/assign token
-    const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
-    res.header('auth-token', token).send(token);
-
+	//create/assign token
+	const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+	res.header("auth-token", token).send(token);
 });
 
 module.exports = router;
--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,24 +1,48 @@
-const Project = require('../model/Project');
-const router = require('express').Router();
+const Project = require("../model/Project");
+const router = require("express").Router();
 
-router.post('/add', async (req,res) => {
+router.post("/add", async (req, res) => {
+	if (!req.body.projectTitle)
+		return res.status(400).send("Missing project title");
 
+	const project = new Project({
+		projectTitle: req.body.projectTitle,
+		description: req.body.description,
+		userList: [req.body.currentUser],
+	});
+	try {
+		const savedProject = await project.save();
+		res.send(savedProject);
+	} catch (err) {
+		res.status(400).send(err);
+		console.log(err);
+	}
+});
 
-    if(!req.body.projectTitle) return res.status(400).send('Missing project title');
+router.get("/get", async (req, res) => {
+	//query param for the project id
+	let projectid = req.query.projectid;
+	const projectExist = await Project.findOne({ _id: projectid });
+	if (!projectExist) return res.status(400).send("Project does not exist");
 
+	res.send(projectExist);
+});
 
-    const project = new Project({
-        projectTitle: req.body.projectTitle,
-        description: req.body.description,
-        userList: [req.body.currentUser],       //FIX
-    });
-    try{
-        const savedProject = await project.save();
-        res.send(savedProject);
-    }catch(err){
-        res.status(400).send(err);
-        console.log(err);
-    }
+router.put("/editproject", async (req, res) => {
+	let savedProject = Project.findOne({ _id: req.body.projectid });
+	savedProject.projectTitle = req.body.projectTitle;
+	savedProject.projectDescription = req.body.projectDescription;
+	await savedProject.save();
+	res.send(savedProject);
+});
+
+router.post("/adduser", async (req, res) => {
+	let workingProject = await Project.findOne({ _id: projectid });
+	req.body.user.forEach((userid) => {
+		workingProject.userList = workingProject.userList.push(userid);
+	});
+	await workingProject.save();
+	res.send(workingProject);
 });
 
 module.exports = router;
